b890351ca06d73759f2c1d37b52364c260e595dd
Or Rubin <or.rubin@hotmail.com>
2018-03-16 15:58:45 +0200

    Add basic server

---
+++ index.js
@@ -0,0 +1,29 @@
+// index.js
+const path = require('path')
+const exphbs = require('express-handlebars')
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.engine('.hbs', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs',
+  layoutsDir: path.join(__dirname, 'views/layouts')
+}))
+app.set('view engine', '.hbs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/', (request, response) => {
+    response.render('layouts/home', {
+      name: 'Backstage Server'
+    })
+  })
+  
+app.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err)
+    }
+
+    console.log(`server is listening on ${port}`)
+})
