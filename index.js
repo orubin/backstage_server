fdf6ed2e5a9b5aaa990d1951c14ae984d0fba8c2
Or Rubin <or.rubin@hotmail.com>
2018-03-22 15:45:16 +0200

    Add Cassandra DB

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 // index.js
 const path = require('path')
 const exphbs = require('express-handlebars')
-
+const db_actions = require('./db_actions');
+var cassandra = require('cassandra-driver');
 const express = require('express')
 const app = express()
 const port = 3000
+
+//Connect to the cluster
+var client = new cassandra.Client({contactPoints: ['127.0.0.1'], keyspace: 'backstage_db'});
 
 app.engine('.hbs', exphbs({
   defaultLayout: 'main',
@@ -19,10 +23,26 @@ app.get('/', (request, response) => {
       name: 'Backstage Server'
     })
   })
+
+app.post('/user', function (req, res) {
+	if(typeof(req.body.UserEdit) != "undefined"){
+		db_actions.UpdateUser(req, res, client);
+	} else {
+		db_actions.InsertUser(req, res, client);
+	}
+});
+
+app.post('/delete_user', function (req, res) {	
+	db_actions.DeleteUser(req, res, client);
+});
+
+app.get('/load_user', function (req, res) {
+	db_actions.LoadUser(req, res, client);
+});
   
 app.listen(process.env.PORT || 5000, (err) => {
     if (err) {
-        return console.log('something bad happened', err)
+        return console.log('Something bad happened', err)
     }
 
     console.log(`server is listening on ${port}`)
