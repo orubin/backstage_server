8674f742050ea0e1efc9681862c3d8afd93602e9
Or Rubin <or.rubin@hotmail.com>
2018-03-28 17:57:19 +0300

    Add creators files

---
+++ creator_db_actions.js
@@ -0,0 +1,83 @@
+/*Creator Table:
+    1. Name - String
+    2. Email - String
+    3. Password - String
+    4. Profile Image - Image
+    5. Rewards - Array of Rewards
+    6. Posts - Array of Posts
+    7. Supporters - Array of Supporters
+    8. PayPal Account - String (email)
+    9. Description - String
+    10. Welcome Video - String(Youtube Link)
+
+    Reward Table:
+    1. Name - String
+    2. Image - String
+    3. Price - Number
+    4. Description - String
+*/
+
+module.exports = {
+	InsertCreator : function (req, res, client){
+        const query = 'INSERT INTO creators (email, name, phone, address, city) VALUES (?,?,?,?,?)'; 
+        const params = [ new Date(1942, 10, 1), 'jimi-hendrix' ];
+		client.execute(function(err, keyspace){
+		    if(err){
+		    	throw(err);
+		    } else {
+		    	var post = req.body;
+				client.cql("INSERT INTO creators (email, name, phone, address, city) VALUES (?,?,?,?,?)", [post.email, post.name, post.phone, post.address, post.city], function(err, results){
+					res.redirect('/');
+				});
+		    }
+        });
+        // Set the prepare flag in the query options
+        client.execute(query, params, { prepare: true })
+        .then(result => console.log('Row updated on the cluster'));
+	},
+	UpdateCreator : function (req, res, client){
+        var post = req.body;
+        const query = 'UPDATE creators SET name = ?, phone = ?, address = ?, city = ? WHERE id = ?'; 
+        const params = [ post.name, post.phone, post.address, post.city, post.id ];
+		client.execute(function(err, keyspace){
+		    if(err){
+		    	throw(err);
+		    } else {
+		    	var post = req.body;
+				client.cql("UPDATE creators SET name = ?, phone = ?, address = ?, city = ? WHERE id = ?", [post.name, post.phone, post.address, post.city, post.id], function(err, results){
+					res.redirect('/');
+				});
+		    }
+		});
+	},
+	DeleteCreator : function (req, res, client){
+        var post = req.body;
+        const query = 'DELETE FROM creators WHERE id = ?'; 
+        const params = [ post.id ];
+        // Set the prepare flag in the query options
+        client.execute(query, params, { prepare: true })
+	},
+	LoadCreator : function (req, res, client){
+		client.execute(function(err, keyspace){
+		    if(err){
+		    	throw(err);
+		    } else {
+		    	var query = "SELECT * FROM creators";
+		    	if(typeof(req.query.id) != "undefined"){
+		    		query += " WHERE id = '"+req.query.id+"'";
+		    	}
+				client.cql(query, [], function(err, results){
+					var data = [];
+					results.forEach(function(row){
+						var obj = {};
+					  	row.forEach(function(name,value,ts,ttl){
+					    	obj[name] = value;
+					  	});
+					  	data.push(obj);
+					});
+					res.send(data);
+				});
+		    }
+		});
+	}
+}
---
+++ user_db_actions.js
@@ -0,0 +1,64 @@
+module.exports = {
+	InsertUser : function (req, res, client){
+        const query = 'INSERT INTO users (email, name, phone, address, city) VALUES (?,?,?,?,?)'; 
+        const params = [ new Date(1942, 10, 1), 'jimi-hendrix' ];
+		client.execute(function(err, keyspace){
+		    if(err){
+		    	throw(err);
+		    } else {
+		    	var post = req.body;
+				client.cql("INSERT INTO users (email, name, phone, address, city) VALUES (?,?,?,?,?)", [post.email, post.name, post.phone, post.address, post.city], function(err, results){
+					res.redirect('/');
+				});
+		    }
+        });
+        // Set the prepare flag in the query options
+        client.execute(query, params, { prepare: true })
+        .then(result => console.log('Row updated on the cluster'));
+	},
+	UpdateUser : function (req, res, client){
+        var post = req.body;
+        const query = 'UPDATE users SET name = ?, phone = ?, address = ?, city = ? WHERE email = ?'; 
+        const params = [ post.name, post.phone, post.address, post.city, post.email ];
+		client.execute(function(err, keyspace){
+		    if(err){
+		    	throw(err);
+		    } else {
+		    	var post = req.body;
+				client.cql("UPDATE users SET name = ?, phone = ?, address = ?, city = ? WHERE email = ?", [post.name, post.phone, post.address, post.city, post.usersEdit], function(err, results){
+					res.redirect('/');
+				});
+		    }
+		});
+	},
+	DeleteUser : function (req, res, client){
+        var post = req.body;
+        const query = 'DELETE FROM users WHERE lastname = ?'; 
+        const params = [ post.lastname ];
+        // Set the prepare flag in the query options
+        client.execute(query, params, { prepare: true })
+	},
+	LoadUser : function (req, res, client){
+		client.execute(function(err, keyspace){
+		    if(err){
+		    	throw(err);
+		    } else {
+		    	var query = "SELECT * FROM users";
+		    	if(typeof(req.query.email) != "undefined"){
+		    		query += " WHERE email = '"+req.query.email+"'";
+		    	}
+				client.cql(query, [], function(err, results){
+					var data = [];
+					results.forEach(function(row){
+						var obj = {};
+					  	row.forEach(function(name,value,ts,ttl){
+					    	obj[name] = value;
+					  	});
+					  	data.push(obj);
+					});
+					res.send(data);
+				});
+		    }
+		});
+	}
+}
