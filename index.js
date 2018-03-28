83b880698eea0afb94a9b32b31ff693be887ad74
Or Rubin <or.rubin@hotmail.com>
2018-03-28 17:57:12 +0300

    Add creators

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // index.js
 const path = require('path')
 const exphbs = require('express-handlebars')
-const db_actions = require('./db_actions');
+const user_db_actions = require('./user_db_actions');
+const creator_db_actions = require('./creator_db_actions');
 var cassandra = require('cassandra-driver');
 const express = require('express')
 const app = express()
@@ -15,6 +16,9 @@ app.engine('.hbs', exphbs({
   extname: '.hbs',
   layoutsDir: path.join(__dirname, 'views/layouts')
 }))
+
+app.use(express.json());
+
 app.set('view engine', '.hbs')
 app.set('views', path.join(__dirname, 'views'))
 
@@ -24,26 +28,38 @@ app.get('/', (request, response) => {
     })
   })
 
+// Users
 app.post('/user', function (req, res) {
 	if(typeof(req.body.UserEdit) != "undefined"){
-		db_actions.UpdateUser(req, res, client);
+		user_db_actions.UpdateUser(req, res, client);
 	} else {
-		db_actions.InsertUser(req, res, client);
+		user_db_actions.InsertUser(req, res, client);
 	}
 });
-
 app.post('/delete_user', function (req, res) {	
-	db_actions.DeleteUser(req, res, client);
+	user_db_actions.DeleteUser(req, res, client);
+});
+app.get('/load_user', function (req, res) {
+	user_db_actions.LoadUser(req, res, client);
 });
 
-app.get('/load_user', function (req, res) {
-	db_actions.LoadUser(req, res, client);
+// Creators
+app.get('load_creator', function (req, res) {
+    creator_db_actions.LoadCreator(req, res, client);
+});
+app.post('insert_creator', function (req, res) {
+    creator_db_actions.InsertCreator(req, res, client);
+});
+app.post('update_creator', function (req, res) {
+    creator_db_actions.UpdateCreator(req, res, client);
+});
+app.post('delete_creator', function (req, res) {
+    creator_db_actions.DeleteCreator(req, res, client);
 });
   
-app.listen(process.env.PORT || 5000, (err) => {
+app.listen(port, (err) => {
     if (err) {
         return console.log('Something bad happened', err)
     }
-
     console.log(`server is listening on ${port}`)
 })
