a7495edf1df2df642f81c29736b978a87650fc99
Or Rubin <or.rubin@hotmail.com>
2018-06-17 16:23:22 +0300

    Add docker file and db changes

--- models/CategoryModel.js
+++ models/CategoryModel.js
@@ -2,7 +2,7 @@ module.exports = {
     fields: {
         id: "uuid",
         name: "text",
-        img_src: "text",
+        img: "text",
         description: "text",
         created_at: "timestamp",
         updated_at: "timestamp"
