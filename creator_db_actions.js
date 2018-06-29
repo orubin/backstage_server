efd057629dbd91a4e8f299fdbc27e2f2cf05e9d5
Or Rubin <or.rubin@hotmail.com>
2018-06-29 13:56:32 +0300

    Add missing images and db work

--- creator_db_actions.js
+++ creator_db_actions.js
@@ -23,33 +23,43 @@ models.setDirectory( __dirname + '/models').bind(
 
 module.exports = {
 	InsertCreator : function (req, res, client){
-	
-        const query = 'INSERT INTO creators (email, name, phone, address, city) VALUES (?,?,?,?,?)'; 
+		var post = req.body;
         const params = [ post.email, post.name, post.phone, post.address, post.city ];
-		client.execute(function(err, keyspace){
-		    if(err){
-		    	throw(err);
-		    } else {
-		    	var post = req.body;
-				client.cql("INSERT INTO creators (email, name, phone, address, city) VALUES (?,?,?,?,?)", [post.email, post.name, post.phone, post.address, post.city], function(err, results){
-					res.redirect('/');
-				});
-		    }
-        });
-        // Set the prepare flag in the query options
-        client.execute(query, params, { prepare: true })
-        .then(result => console.log('Row updated on the cluster'));
+		var creator = new models.instance.Creator({
+			id: models.uuid(),
+			name: post.name,
+			username: post.username,
+			email: post.email,
+			password: '',
+			paypal_address: post.paypal_address,
+			tagline: post.tagline,
+			overview: post.overview,
+			video: 'https://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4',
+			sponsors: 0,
+			monthly_income: 0,
+			profile: '',
+			cover_picture: '',
+			category_id: 0,
+			updated_at: Date.now(),
+			created_at: Date.now()
+		});
+
+		creator.save(function(err){
+			if(err) {
+				console.log(err);
+			}
+		});
 	},
 	UpdateCreator : function (req, res, client){
         var post = req.body;
-        const query = 'UPDATE creators SET name = ?, phone = ?, address = ?, city = ? WHERE id = ?'; 
+        const query = 'UPDATE creator SET name = ?, phone = ?, address = ?, city = ? WHERE id = ?'; 
         const params = [ post.name, post.phone, post.address, post.city, post.id ];
 		client.execute(function(err, keyspace){
 		    if(err){
 		    	throw(err);
 		    } else {
 		    	var post = req.body;
-				client.cql("UPDATE creators SET name = ?, phone = ?, address = ?, city = ? WHERE id = ?", [post.name, post.phone, post.address, post.city, post.id], function(err, results){
+				client.cql("UPDATE creator SET name = ?, phone = ?, address = ?, city = ? WHERE id = ?", [post.name, post.phone, post.address, post.city, post.id], function(err, results){
 					res.redirect('/');
 				});
 		    }
@@ -57,8 +67,8 @@ module.exports = {
 	},
 	DeleteCreator : function (req, res, client){
         var post = req.body;
-        const query = 'DELETE FROM creator WHERE id = ?'; 
-        const params = [ post.id ];
+        const query = 'DELETE FROM creator WHERE username = ?'; 
+        const params = [ post.username ];
         // Set the prepare flag in the query options
         client.execute(query, params, { prepare: true })
 	},
@@ -73,7 +83,7 @@ module.exports = {
 			if (result !== undefined){
 				var data = JSON.stringify(result.rows[0]);
 				var obj = JSON.parse(data);
-				const query2 = 'select * from backstage_db.creatorrewards where creator_username = ? ALLOW FILTERING';
+				const query2 = 'select * from creatorrewards where creator_username = ? ALLOW FILTERING';
 				client.execute(query2, params, { prepare: true }, function (err, result) {
 					if(err) {
 						console.log(err);
@@ -91,13 +101,6 @@ module.exports = {
 				//return res(null, JSON.stringify(result.rows[0]));
 			}
 		 });
-		//return '{"name":"Kevin Stewart", "description":"internet videos and podcasts", "profile_picture":"https://lorempixel.com/400/200/people", "cover_picture":"https://lorempixel.com/400/200/people", "intro_video":"https://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"}';
-		// var id = 1;
-		// if (id<2) {
-		// 	return res(null, '{"name": "Hadag Nahash", "username": "hadagnahash", "email": "hadagnahash@backstage.com", "paypal": "hadagnahash@gmail.com", "tagline": "#1 Hip Hop Band in the Middle East", "overview": "Hadag Nahash has been a major contributor to the Israeli hip-hop scene, and is presently one of Israels most successful bands, with seven studio albums released to date. The bands songs call for peace, tolerance and equality, and include political and social protest. Most songs are written by Shaanan Street, the bands lead vocalist.", "video": "https://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4","sponsors": 680,"monthly_income": 4500,"": "","profile": "https://cdn.thinglink.me/api/image/608247153780850690/1240/10/scaletowidth","cover_picture":"https://lorempixel.com/400/200/city","category": {"type": "Music","img": "https://musicaustralia.org.au/wp-content/uploads/2017/02/livemusic-portdhiver-web-ed.jpg"},"rewards": [{"title": "$5 donation - Starter Bro","price": 5,"description": "Become a Starter Bro of the hadagnahash, you get early access to all the videos we make + weekly updated Spotify secret playlist","img": "https://lorempixel.com/400/200/city"},{"title": "$10 donation - Pro Bro","price": 10,"description": "Become a Pro Bro of the hadagnahash, you get a free ticket to our show when we perform in your city."},{"title": "15 donation - Super Bro","price": 15,"description": "Become a Super Bro of the hadagnahash, you get backstage entry to you and a friend to meet us after the show and talk shit. BYOB.","img": "https://media.istockphoto.com/photos/gift-box-with-white-background-picture-id516815936"},{"title": "$20 donation - True Bro","price": 20,"description": "Become a True Bro of the hadagnahash, you get all rewards + kiss from mooky. <3","img": "https://media.istockphoto.com/photos/gift-box-with-white-background-picture-id516815936"}],"posts": [{"title": "hello Fish Snake","body": "this is our last tour day, see you next year"},{"title": "hello Pish Snake","body": "this is our last tour week, see you next year"},{"title": "hello Kish Snake","body": "this is our last tour month, see you next year"},{"title": "hello Mish Snake", "body": "this is our last tour season, see you next year"}]}');
-		// } else {
-		// 	return res(null, '{"name": "Hadag Nahash", "username": "hadagnahash", "email": "hadagnahash@backstage.com", "paypal": "hadagnahash@gmail.com", "tagline": "#1 Hip Hop Band in the Middle East", "overview": "Hadag Nahash has been a major contributor to the Israeli hip-hop scene, and is presently one of Israels most successful bands, with seven studio albums released to date. The bands songs call for peace, tolerance and equality, and include political and social protest. Most songs are written by Shaanan Street, the bands lead vocalist.", "video": "","sponsors": 680,"monthly_income": 4500,"": "","profile": "https://cdn.thinglink.me/api/image/608247153780850690/1240/10/scaletowidth","cover_picture":"https://lorempixel.com/400/200/city","category": {"type": "Music","img": "https://musicaustralia.org.au/wp-content/uploads/2017/02/livemusic-portdhiver-web-ed.jpg"},"rewards": [{"title": "$5 donation - Starter Bro","price": 5,"description": "Become a Starter Bro of the hadagnahash, you get early access to all the videos we make + weekly updated Spotify secret playlist","img": "https://media.istockphoto.com/photos/gift-box-with-white-background-picture-id516815936"},{"title": "$10 donation - Pro Bro","price": 10,"description": "Become a Pro Bro of the hadagnahash, you get a free ticket to our show when we perform in your city."},{"title": "15 donation - Super Bro","price": 15,"description": "Become a Super Bro of the hadagnahash, you get backstage entry to you and a friend to meet us after the show and talk shit. BYOB.","img": "https://media.istockphoto.com/photos/gift-box-with-white-background-picture-id516815936"},{"title": "$20 donation - True Bro","price": 20,"description": "Become a True Bro of the hadagnahash, you get all rewards + kiss from mooky. <3","img": "https://media.istockphoto.com/photos/gift-box-with-white-background-picture-id516815936"}],"posts": [{"title": "hello Fish Snake","body": "this is our last tour day, see you next year"},{"title": "hello Pish Snake","body": "this is our last tour week, see you next year"},{"title": "hello Kish Snake","body": "this is our last tour month, see you next year"},{"title": "hello Mish Snake", "body": "this is our last tour season, see you next year"}]}');
-		// }
 	},
 	LoadCreators : function (client, res){
 		const query = 'SELECT * FROM creator ALLOW FILTERING';
@@ -154,7 +157,7 @@ module.exports = {
 				sponsors: parseInt(5000 / (i+1)),
 				monthly_income: parseInt(25000 / (i+1)),
 				profile: 'https://cdn.thinglink.me/api/image/608247153780850690/1240/10/scaletowidth',
-				cover_picture: 'https://lorempixel.com/400/200/city',
+				cover_picture: '../city.jpeg',
 				category_id: 1,
 				updated_at: Date.now(),
 				created_at: Date.now()
@@ -170,7 +173,7 @@ module.exports = {
 		var rewards_titles = ['$5 donation - Starter Bro', '$10 donation - Pro Bro', '15$ donation - Super Bro', '$20 donation - True Bro'];
 		var rewards_price = [5, 10, 15, 20];
 		var rewards_description = ['Become a Starter Bro: you get early access to all the videos we make + weekly updated Spotify secret playlist', 'Become a Pro Bro: you get a free ticket to our show when we perform in your city.', 'Become a Super Bro: you get backstage entry to you and a friend to meet us after the show and talk shit. BYOB.', 'Become a True Bro: you get all rewards + kiss from mooky'];
-		var rewards_images = ['https://lorempixel.com/400/200/city', 'https://lorempixel.com/400/200/city', 'https://media.istockphoto.com/photos/gift-box-with-white-background-picture-id516815936', 'https://media.istockphoto.com/photos/gift-box-with-white-background-picture-id516815936'];
+		var rewards_images = ['../city.jpeg', '../city.jpeg', 'https://media.istockphoto.com/photos/gift-box-with-white-background-picture-id516815936', 'https://media.istockphoto.com/photos/gift-box-with-white-background-picture-id516815936'];
 
 		for (var j = 0; j < creators_usernames.length-1; j++) {
 			for (i = 0; i < rewards_titles.length; i++) {
